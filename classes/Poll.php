085f53d8716567662bcb89f5713084a56d7f0095
webdocs <webdocs@mail.ru>
2022-11-29 13:50:55 +0500

    poll

--- classes/Poll.php
+++ classes/Poll.php
@@ -3,19 +3,14 @@
 class Poll{
 	
 	public static function add($user_from,$user_to){
-		$result = DB::query("INSERT INTO polls SET user_from=:user_from, user_to=:user_to",['user_from'=>$user_from,'user_to'=>$user_to]);
-		echo 'add poll ';
-		d($result);
+		$result = DB::query("INSERT INTO polls SET user_from=:user_from, user_to=:user_to, created_at=NOW()",['user_from'=>$user_from,'user_to'=>$user_to]);
 		return $result;
 	}
 
-	/*public static function get($id){
-	    $user = DB::select("SELECT * FROM users WHERE id={$id} LIMIT 1");
+	public static function stat($id){
+	    $user = DB::select("SELECT * FROM polls WHERE id={$id} LIMIT 1");
 	    return $user[0];
     }
-	public static function getByLogin($login){
-	    $user = DB::select("SELECT * FROM users WHERE phone='{$login}' LIMIT 1");
-	    return $user[0];
-    }*/
+
 
 }
