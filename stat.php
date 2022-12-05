5825a73f2532de2f643e3551a7f2656abc1a3f13
web-docs <webdocs@mail.ru>
2022-12-05 14:29:58 +0300

    update

--- stat.php
+++ stat.php
@@ -1,12 +1,42 @@
 <?php
 
 require ('init.php');
-
+/*
 if(!Auth::check()){
     Auth::redirect('login.php');
+} */
+
+
+$users = Poll::stat();
+$cnt = 0;
+foreach($users as $user) {
+    $cnt += $user['cnt'];
 }
+?>
+    <h3>Всего голосов: <?=$cnt ?></h3>
+    <table class="table">
+        <tr>
+            <th>#</th>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Голосов</th>
+        </tr>
 
+        <?php
+        $n=1;
+        foreach($users as $user){ ?>
+            <tr>
+                <th><?=$n ?></th>
+                <th><?=$user['firstname']?></th>
+                <th><?=$user['lastname']?></th>
+                <th><?=$user['cnt']?></th>
 
-$result = Poll::stat();
+            </tr>
+        <?php
+        $n++;
+        } ?>
 
-d($result);
+    </table>
+
+<?php
+
