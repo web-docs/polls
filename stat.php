4e8058d49e8117c38f7919d2a1e0607344489e48
web-docs <webdocs@mail.ru>
2022-12-07 10:33:49 +0500

    complete

--- stat.php
+++ stat.php
@@ -18,8 +18,18 @@ foreach($users as $user) {
     <html>
     <head>
         <meta charset="UTF-8">
-        <title>Bucheon University in Tashkent | Complete</title>
+        <title>Bucheon University in Tashkent | Results</title>
         <link rel="stylesheet" href="assets/css/app.css">
+        <style>
+            #list_position_1,
+            #list_position_2,
+            #list_position_3{
+                background: #fff;
+                margin:5px;
+                padding: 5px;
+            }
+
+        </style>
     </head>
     <body>
     <div class="snowing">
@@ -115,49 +125,46 @@ foreach($users as $user) {
     <div class="juniper">
 
         <h1>Результаты голосования</h1>
-
+        <div>
+            <h2>Всего голосов: <?=$cnt ?></h2>
+        </div>
         <div class="juniper-img">
             <img src="assets/img/juniper-claus.png" alt="">
         </div>
     </div>
 
 <div>
-    <div>
-        <h1>Всего голосов: <?=$cnt ?></h1>
-    </div>
-    <div style="clear: both">
 
 
+        <?php // <a href="/?logout">Выйти</a>
+        $limit = 10000;
+        $n=1;
+        $u = [1=>0,2=>0,3=>0];
+        $old_position = '';
+        foreach($users as $user){
+            if( $user['position_id'] != $old_position ) {
+                $old_position = $user['position_id'];
+                if($old_position!='') echo '</div>';
+                echo '<div id="list_position_'.$user['position_id'].'" class="user-list">';
+                $n=1;
+            }
+            $u[$user['position_id']]++;
+            if($u[$user['position_id']]>$limit) continue;
 
+            ?>
+            <div class="user-item">
+                <div><?=$n . '. ' . $user['fio_passport'] . ' ' . $user['cnt'] ?></div>
+                <?php /*<img src="/assets/img/<?=$user['id'] ?>.jpg" width="48px" /><img src="/assets/img/juniper-claus.png" height="32px" /> */ ?>
 
+            </div>
             <?php
-            $n=1;
-            $u = [1=>0,2=>0,3=>0];
-            $old_position = '';
-            foreach($users as $user){
-                if( $user['position_id'] != $old_position ) {
-                    $old_position = $user['position_id'];
-                    if($old_position!='') echo '</div>';
-                    echo '<div id="position_'.$user['position_id'].'" class="user-list">';
-                }
-                $u[$user['position_id']]++;
-                if($u[$user['position_id']]>2) continue;
+            $n++;
 
-                ?>
-                <div class="user-item">
-                    <div><?=$n . ' ' . $user['fio_passport'] . ' ' . $user['cnt'] . ' ' . $user['position_id'] ?></div>
-                    <td><img src="/assets/img/<?=$user['id'] ?>.jpg" width="48px" /><img src="/assets/img/juniper-claus.png" height="32px" /> </td>
-
-                </div>
-                <?php
-                $n++;
-
-            } ?>
-
-        </div>
+        } ?>
+    </div>
 
 
-        <table class="table">
+        <?php /*<table class="table">
             <tr>
                 <th>#</th>
                 <th>Фото</th>
@@ -194,8 +201,8 @@ foreach($users as $user) {
 
             } ?>
 
-        </table>
-    </div>
+        </table> */ ?>
+
 </div>
 
 
