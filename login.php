2b34a720f32725d7dd73399f61e4e5ae0f9eb61a
khamza <khamza.khudayberdiyev@gmail.com>
2022-12-07 11:28:32 +0500

    test

--- login.php
+++ login.php
@@ -37,122 +37,124 @@ if (isset($_POST['login'])) {
 
 
 
-<div class="snowing">
-  <div class="small-snow-left">
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-  </div>
-  <div class="small-snow-right">
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-    <div class="small"></div>
-  </div>
-  <div class="medium-snow-left">
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-  </div>
-  <div class="medium-snow-right">
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-    <div class="medium"></div>
-  </div>
-</div>
-
-<?php include ('alert.php') ?>
-
-<div class="login">
-  <div class="container">
-    <div class="login-wrapper">
-      <div class="login-img">
-        <img src="assets/img/login.svg" alt="">
-        <form action="" method="POST" class="login-form">
-          <div class="form-group">
-            <label class="form-label">Телефон</label>
-            <div class="login-icon">
-              <input type="text" name="login" class="form-control phone-mask" placeholder="Введите номер телефона без знака +">
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="form-label">Пароль</label>
-            <div class="password-icon">
-              <input type="password" name="password" class="form-control" placeholder="Введите пароль">
-            </div>
-          </div>
-          <input type="submit" value="Войти" class="login-submit">
-        </form>
-      </div>
+<div class="login-wrapper">
+    <div class="snowing">
+        <div class="small-snow-left">
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+        </div>
+        <div class="small-snow-right">
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+            <div class="small"></div>
+        </div>
+        <div class="medium-snow-left">
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+        </div>
+        <div class="medium-snow-right">
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+            <div class="medium"></div>
+        </div>
     </div>
-  </div>
+
+    <?php include ('alert.php') ?>
+
+    <div class="login">
+        <div class="container">
+            <div class="login-wrapper">
+                <div class="login-img">
+                    <img src="assets/img/login.svg" alt="">
+                    <form action="" method="POST" class="login-form">
+                        <div class="form-group">
+                            <label class="form-label">Телефон</label>
+                            <div class="login-icon">
+                                <input type="text" name="login" class="form-control phone-mask" placeholder="Введите номер телефона без знака +">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label">Пароль</label>
+                            <div class="password-icon">
+                                <input type="password" name="password" class="form-control" placeholder="Введите пароль">
+                            </div>
+                        </div>
+                        <input type="submit" value="Войти" class="login-submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script src="assets/js/jquery-2.0.3.min.js"></script>
