621318c51888b20113f68dee80d7428985e7f7b8
web-docs <webdocs@mail.ru>
2022-12-07 21:44:11 +0500

    stat

--- stat.php
+++ stat.php
@@ -11,6 +11,23 @@ $cnt = 0;
 foreach ($users as $user) {
     $cnt += $user['cnt'];
 }
+$limit = 1; // 1 победитель в номинации
+$n=1;
+$u = [1=>0,2=>0,3=>0];
+$winner = [];
+$old_position = '';
+foreach($users as $user){
+    if( $user['position_id'] != $old_position ) {
+        $old_position = $user['position_id'];
+        $n=1;
+    }
+    $u[$user['position_id']]++;
+    if(!isset($winner[$user['position_id']])) $winner[$user['position_id']] = $user;
+    if($u[$user['position_id']]>$limit) continue;
+
+$n++;
+
+}
 ?>
 
 
@@ -128,29 +145,29 @@ foreach ($users as $user) {
             <div class="winner-wrapper">
                 <div class="winner-user">
                     <div class="winner-user__img">
-                        <img src="assets/img/juniper-claus.png" alt="">
+                        <img src="assets/img/juniper-claus.png?<?=isset($winner[2]) ? $winner[2]['phone'] : '' ?>.jpg" alt="">
                     </div>
-                    <h3>Альфредо Припидучи</h3>
+                    <h3 data-id="<?=$winner[2]['id']?>"><?=isset($winner[2]) ? $winner[2]['fio_passport']  : 'Не определен'?></h3>
                     <span>
                     Сотрудник года
                 </span>
                 </div>
                 <div class="winner-user">
                     <div class="winner-user__img">
-                        <img src="assets/img/juniper-claus.png" alt="">
+                        <img src="assets/img/juniper-claus.png?<?=isset($winner[1]) ? $winner[1]['phone'] : ''?>.jpg" alt="">
                     </div>
-                    <h3>Альфредо Припидучи</h3>
+                    <h3 data-id="<?=$winner[1]['id']?>"><?=isset($winner[1]) ? $winner[1]['fio_passport'] : 'Не определен'?></h3>
                     <span>
-                    Сотрудник года
+                    Начальник года
                 </span>
                 </div>
                 <div class="winner-user">
                     <div class="winner-user__img">
-                        <img src="assets/img/juniper-claus.png" alt="">
+                        <img src="assets/img/juniper-claus.png?<?=isset($winner[3]) ? $winner[3]['phone'] : ''?>.jpg" alt="">
                     </div>
-                    <h3>Альфредо Припидучи</h3>
+                    <h3 data-id="<?=$winner[3]['id']?>"><?=isset($winner[3]) ? $winner[3]['fio_passport'] : 'Не определен'?></h3>
                     <span>
-                    Сотрудник года
+                    Хоз года
                 </span>
                 </div>
             </div>
