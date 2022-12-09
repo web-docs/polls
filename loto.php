175ba3107a106757d83c9303b82bf6827c927c05
web-docs <webdocs@mail.ru>
2022-12-09 20:35:47 +0500

    loto split digits

--- loto.php
+++ loto.php
@@ -1,3 +1,9 @@
+<?php
+
+$count = isset($_GET['c']) ? $_GET['c'] : 150;
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,17 +13,17 @@
   <title>Loto</title>
 </head>
 <style>
-  .generate {
-    /* Remove button styles */
-    border: none;
-    background: transparent;
-    outline: none;
-    /*overflow: none;*/
-    /* Make 'clickable' */
-    cursor: pointer;
-  }
 
-  .number {
+
+  .number{
+      /* Remove button styles */
+      border: none;
+      background: transparent;
+      outline: none;
+      /*overflow: none;*/
+      /* Make 'clickable' */
+      cursor: pointer;
+
     text-shadow: 0 0 5px #bbb;
     /* Make it big */
     font-weight: 900;
@@ -34,27 +40,23 @@
     right: 0;
   }
 
-  /*.options {*/
-  /*  background-color: #555;*/
-  /*  border-radius: 5px;*/
-  /*  color: #efefef;*/
-  /*  padding: 10px 20px;*/
-  /*  position: absolute;*/
-  /*  cursor: pointer;*/
-  /*}*/
-  .options input, .options label {
-    display: none;
+  .spinner{
+      text-shadow: 0 0 5px #bbb;
+      /* Make it big */
+      font-weight: 900;
+      font-size: 250px;
+      /* Center it */
+      width: 50%;
+      min-height: 50%;
+      overflow: auto;
+      margin: auto;
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
   }
-  .options:hover input, .options:hover label {
-    display: inline;
-  }
-  .options input {
-    background-color: #efefef;
-    padding: 10px;
-    margin: 5px 15px;
-  }
-  
-  #results_block{
+  #results{
       background:#ffcc00;
       font-size:48px;
       width:200px;
@@ -62,80 +64,95 @@
       margin:10px;
       
   }
-  
+  #results_1,
+  #results_2,
+  #results_3{
+      border: 1px solid #ccc;
+      margin: 2px;
+  }
 </style>
 <body>
-<button class="generate number">START</button>
 
-<div id="results_block">
-    <span id="results"></span>
+<div class="spinner">
+    <span id="results_1">0</span>
+    <span id="results_2">0</span>
+    <span id="results_3">0</span>
 </div>
 
+<button class="generate number">
+    START
+</button>
+
+
+<div id="results">
+    <span id="result_list"></span>
+
+</div>
+<?php /*
+<a href="/loto.php?c=999">Пример задания диапазона пользователей от 1 до 999  ?c=999 </a>
+*/ ?>
 
 <script>
-    var generator = {
-        min: 1,
-        max: 150,
-
-        /**
-         * Сгенерировать случайное число
-         */
-        getRandom: function () {
-            return Math.round(Math.random() * (this.max - this.min) + this.min);
-        },
-
-        /**
-         * Установите нижнюю границу числа
-         */
-        setMin: function (newMin) {
-            this.min = parseInt(newMin);
-        },
-
-        /**
-         * Установите верхнюю границу числа
-         */
-        setMax: function (newMax) {
-            this.max = parseInt(newMax);
-        }
-    };
-
-    var number = document.querySelector('.number');
+    var min = 1;
+    var max = '<?=$count ?>';
     var k=0;
     var numbers = [];
-    var results = document.querySelector('#results');
+    var results = document.querySelector('#result_list');
+    var results_1 = document.querySelector('#results_1');
+    var results_2 = document.querySelector('#results_2');
+    var results_3 = document.querySelector('#results_3');
+
     document.querySelector('.generate').onclick = function () {
-        number.textContent = generator.getRandom();
-        var time = 3000;
-        var delay = 100;
+        var time = 5000;
+        var delay = 80;
         var timerId ;
+        var res= [];
         timerId = setInterval(function random(){
            time-=delay; 
-           num = generator.getRandom();
-           while( numbers.indexOf(num) >= 0 ){
-               num = generator.getRandom();               
-           }
-           
-           number.textContent = num;
-           
+           num = getRandom();
+
+            res = correctNumber(num);
+            results_1.innerHTML= res[0];
+            results_2.innerHTML= res[1];
+            results_3.innerHTML= res[2];
+
            if(time<=0) {               
                clearInterval(timerId);
                k++;
-               results.innerHTML+= k+'. <b>' + num +'</b><br>'; 
+
+               while (numbers.indexOf(num) >= 0) {
+
+                   if(numbers.length==max) {
+                       num=0;
+                       break;
+                   }
+                   num = getRandom();
+               }
+               res = correctNumber(num);
+               results_1.innerHTML = res[0];
+               results_2.innerHTML = res[1];
+               results_3.innerHTML = res[2];
+               if(num>0) {
+                   numbers.push(num);
+                   results.innerHTML += k + '. <b>' + num + '</b><br>';
+               }
            }
         }, delay);
-        
+
     };
 
-    var options = document.querySelector('.options');
+    function getRandom(){
+        return Math.round(Math.random() * (max - min) + min);
+    }
 
-    options.querySelector('#min').onchange = function () {
-        generator.setMin(this.value);
-    };
-
-    options.querySelector('#max').onchange = function () {
-        generator.setMax(this.value);
-    };
-
+    function correctNumber(num){
+        if(num<10){
+            num = '00' + num;
+        }else if(num>=10 && num <100){
+            num = '0' + num;
+        }
+        return (""+num).split("");
+    }
 
 </script>
 </body>
