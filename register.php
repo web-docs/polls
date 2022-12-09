ff289e69acec6815e0ff163aec066eb681c9e916
khamza <khamza.khudayberdiyev@gmail.com>
2022-12-09 16:12:25 +0500

    add bootstrap

--- register.php
+++ register.php
@@ -22,7 +22,12 @@ if (isset($_POST['register'])) {
 <head>
   <meta charset="UTF-8">
   <title>Bucheon University in Tashkent | Регистрация</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/app.css">
+  <link rel="stylesheet" href="assets/css/media.css">
   <style>
     .alert {
       background: #8b1111;
@@ -89,5 +94,6 @@ if ($error) { ?>
   </form>
 </div>
 
+<script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
